3aeb8cfc8fed92b6e7a3ef3b5be155521affdfe9
aina <aina.xu@rib-software.com>
2020-06-01 14:37:03 +0800

    modify files

--- FlaskSQLAIChemy/app.py
+++ FlaskSQLAIChemy/app.py
@@ -1,20 +1,11 @@
 from flask import Flask,request,flash,render_template,redirect,url_for
-from flask_sqlalchemy import SQLAlchemy
-#from Flask_Framework import train
 
 app = Flask('Flask Framework')
-# db=SQLAlchemy(app)
-#
-# app.config['SQLALCHEMY_DATABASE_URI']='sqlserver:'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
 app.secret_key='aina_flask_framework'
 
 @app.route('/',methods=['GET','POST'])
 def submit():
-    #x, y, x_test, y_test=train.loadData(True)
-    #train.trainRegression(x, y, x_test, y_test)
-    #train.trainNN(x, y, x_test, y_test)
-    #train.trainCNN(x, y, x_test, y_test)
     if request.method=='POST':
         username=request.form.get('username')
         password=request.form.get('password')
