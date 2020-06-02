b70bbf04e707199975bb37681e635acb32f48322
aina <aina.xu@rib-software.com>
2020-06-02 11:42:00 +0800

    delete login.html

--- FlaskSQLAIChemy/app.py
+++ FlaskSQLAIChemy/app.py
@@ -1,21 +1,11 @@
-from flask import Flask,request,flash,render_template,redirect,url_for
+from flask import Flask
 
 app = Flask('Flask Framework')
 
-app.secret_key='aina_flask_framework'
 
 @app.route('/',methods=['GET','POST'])
 def submit():
-    if request.method=='POST':
-        username=request.form.get('username')
-        password=request.form.get('password')
-        if not all([username,
-                   password]):
-            flash('user name or password not empty')
-        else:
-            return redirect(url_for('login'))
-
-    return  render_template('login.html')
+    return  'Hello my flask!'
 
 @app.route('/login')
 def login():
